64542361e7e5476963aff374144bc97739f37ce2
Suhyun Kim <ideptor@gmail.com>
2022-02-05 16:21:48 +0900

    random block

--- tdd-tetris/block.py
+++ tdd-tetris/block.py
@@ -1,47 +1,87 @@
+import random
+
 class Block:
 
-    blocks = [ 
-        [
-            [1,1],
-            [1,1]
-        ],
-        [
-            [1,1],
-            [0,1],
-            [0,1]
-        ],
-        [
-            [1,1],
-            [1,0],
-            [1,0]
-        ],
-        [
-            [0,1],
-            [1,1],
-            [0,1]
-        ],
-        [
-            [0,1],
-            [1,1],
-            [1,0]
-        ],
-        [
-            [1,0],
-            [1,1],
-            [0,1]
-        ],
-        [
-            [0,1],
-            [0,1],
-            [0,1],
-            [0,1]
-        ],
-    ]
+    def __init__(self, shape_idx:int = -1):
 
-    def __init__(self, shape_idx:int):
-        if shape_idx >= len(self.blocks):
+        if shape_idx == -1:
+            self.shape_idx = random.randint(0, len(self.blocks)-1)
+        elif shape_idx >= len(self.blocks):
             shape_idx = 0
-        self.shape_idx = shape_idx
+        else:            
+            self.shape_idx = shape_idx
+        
+        self.rotate_idx = 0
 
     def show(self):
-        return self.blocks[self.shape_idx]
+        return self.blocks[self.shape_idx][self.rotate_idx]
+
+    def rotate(self):
+        self.rotate_idx += 1
+        self.rotate_idx %= len(self.blocks[self.shape_idx])
+
+
+    blocks = [
+        [ 
+            [
+                [1,1],
+                [1,1]
+            ],
+        ],
+        [
+            [
+                [1,1],
+                [0,1],
+                [0,1]
+            ],
+            [
+                [0,0,1],
+                [1,1,1],
+            ],
+            [
+                [1,0],
+                [1,0],
+                [1,1]
+            ],
+                    [
+                [1,1,1],
+                [1,0,0],
+            ],
+        ],
+        [
+            [
+                [1,1],
+                [1,0],
+                [1,0]
+            ]
+        ],
+        [
+            [
+                [0,1],
+                [1,1],
+                [0,1]
+            ]
+        ],
+        [
+            [
+                [0,1],
+                [1,1],
+                [1,0]
+            ]
+        ],
+        [
+            [
+                [1,0],
+                [1,1],
+                [0,1]
+            ]
+        ],
+        [
+            [
+                [0,1],
+                [0,1],
+                [0,1],
+                [0,1]
+            ]
+        ],
+    ]
--- tdd-tetris/test/test_block.py
+++ tdd-tetris/test/test_block.py
@@ -47,3 +47,48 @@ def test_block_show():
     for i in range(len(show_blocks)):
         b = Block(i)
         assert b.show() == show_blocks[i]
+
+
+def test_rotate():
+    
+    # given
+    block1 = [
+        [
+            [1,1],
+            [0,1],
+            [0,1]
+        ],
+         [
+            [0,0,1],
+            [1,1,1],
+        ],
+        [
+            [1,0],
+            [1,0],
+            [1,1]
+        ],
+        [
+            [1,1,1],
+            [1,0,0],
+        ],
+    ]
+
+    #when
+    b = Block(1)
+
+    # then
+    for rotated in block1:
+        assert b.show() == rotated
+        b.rotate()
+
+
+def test_random_block():
+    
+    # given
+    # when
+    b = Block()
+
+    # then
+    assert len(b.blocks) > 0
+    assert b.shape_idx >= 0
+    assert b.shape_idx < len(b.blocks)
